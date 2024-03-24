14bec055d3f105ba7067ed662a2bc4a49856399b
mamahoos <m4m4hoos@gmail.com>
2024-03-24 04:47:44 +0330

    Update __init__.py

--- input-with-timeout/__init__.py
+++ input-with-timeout/__init__.py
@@ -1,1 +1,22 @@
+"""
+The 'input-with-timeout' module provides a cross-platform method to capture user input with a timeout feature.
+This is particularly useful for creating command-line interfaces where the program needs to proceed if the user does not provide input within a certain time frame.
+
+This module is part of the 'python-input-with-timeout' project by mamahoos, available at:
+https://github.com/mamahoos/python-input-with-timeout
+
+For any inquiries or contributions, you can reach out via:
+- Telegram: @mamahoos
+- Email: m4m4hoos@gmail.com
+- Github: https://github.com/mamahoos
+
+MIT License:
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
 from .input_with_timeout import input_with_timeout
