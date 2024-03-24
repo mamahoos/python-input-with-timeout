e3c83420092a32bba61d98ad69819025c607207d
mamahoos <m4m4hoos@gmail.com>
2024-03-25 02:03:31 +0330

    Create __version__.py

---
+++ input-with-timeout/__version__.py
@@ -0,0 +1,19 @@
+__title__        = 'input_with_timeout'
+__description__  = 'The \'input-with-timeout\' module provides a cross-platform method to capture user input with a timeout feature.'
+__version__      = '1.0'
+__version_info__ = (1,)
+__author__       = 'mamahoos'
+__author_email__ = 'm4m4hoos@gmail.com'
+__url__          = 'http://github.com/mamahoos/input_with_timeout'
+__copyright__    = 'Copyright (c) 2024 mamahoos'
+__license__      = 'MIT License'
+__keywords__     = ['input', 'timeout', 'user input', 'cross-platform']
+__platforms__    = 'Any'
+__classifiers__  = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Operating System :: OS Independent'
+]
