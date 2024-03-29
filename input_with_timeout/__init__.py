837652fa31c244dcccb20df37fdc1fc9a7dcb405
mamahoos <m4m4hoos@gmail.com>
2024-03-30 01:34:42 +0330

    Update __init__.py

--- input_with_timeout/__init__.py
+++ input_with_timeout/__init__.py
@@ -17,14 +17,6 @@ For any inquiries or contributions, you can reach out via:
 - Telegram: https://t.me/mamahoos
 - Email: m4m4hoos@gmail.com
 - Github: https://github.com/mamahoos
-
-### MIT License:
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
 from .main import input_with_timeout
