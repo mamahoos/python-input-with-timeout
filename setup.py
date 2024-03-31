b7157f212f9c7c29e61a410a8268d624bcb32e47
mamahoos <m.h.kkouhkan@gmail.com>
2024-04-01 00:50:37 +0330

    Updated

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup, find_packages
 from input_with_timeout import (__title__, __version__, __url__,__license__, __author__,
                                 __classifiers__, __author_email__, __description__)
 
+path = '\\'.join(__file__.split('\\')[:-1])
+
 setup(
     name=__title__.replace('_', '-')                                            ,
     version=__version__                                                         ,
@@ -11,7 +13,7 @@ setup(
     author=__author__                                                           ,
     author_email=__author_email__                                               ,
     description=__description__                                                 ,
-    long_description=open('README.md', mode='rt').read()                        ,
+    long_description=open(path + '\\README.md', mode='rt').read()                        ,
     keywords=['python', 'cross-platform', 'stdout', 'stin', 'console']          ,
     classifiers=__classifiers__                                                 ,
 )
