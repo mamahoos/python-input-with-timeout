a27985aaac9651bb07acb1fa27e24ea8e27ada61
mamahoos <m.h.kkouhkan@gmail.com>
2024-04-01 01:03:11 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from input_with_timeout import (__title__, __version__, __url__,__license__, __a
                                 __classifiers__, __author_email__, __description__)
 
 
-# read the contents of your README file
+# read the contents of README.md file
 from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 
 setup(
-    name=__title__.replace('_', '-')                                            ,
+    name=__title__                                       ,
     version=__version__                                                         ,
     packages=find_packages(exclude=(r"python-input-with-timout\old_version", )) ,
     url=__url__                                                                 ,
