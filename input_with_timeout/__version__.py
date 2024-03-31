4d521a4b812d97f5169350e2fda9b7a30cdf758b
mamahoos <m.h.kkouhkan@gmail.com>
2024-04-01 00:54:51 +0330

    Updated

--- input_with_timeout/__version__.py
+++ input_with_timeout/__version__.py
@@ -1,4 +1,4 @@
-__title__        = 'input_with_timeout'
+__title__        = 'input-with-timeout'
 __description__  = 'The \'input-with-timeout\' module provides a cross-platform method to capture user input with a timeout feature.'
 __version__      = '3.0'
 __version_info__ = (3, 0)
--- setup.py
+++ setup.py
@@ -2,7 +2,12 @@ from setuptools import setup, find_packages
 from input_with_timeout import (__title__, __version__, __url__,__license__, __author__,
                                 __classifiers__, __author_email__, __description__)
 
-path = '\\'.join(__file__.split('\\')[:-1])
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 
 setup(
     name=__title__.replace('_', '-')                                            ,
@@ -13,7 +18,8 @@ setup(
     author=__author__                                                           ,
     author_email=__author_email__                                               ,
     description=__description__                                                 ,
-    long_description=open(path + '\\README.md', mode='rt').read()                        ,
+    long_description=long_description                                           ,
+    long_description_content_type='text/markdown'                               ,
     keywords=['python', 'cross-platform', 'stdout', 'stin', 'console']          ,
     classifiers=__classifiers__                                                 ,
 )
