b2396f3ebc2aa72b47d3323dc6acdd0911fbc83b
mamahoos <m.h.kkouhkan@gmail.com>
2024-04-01 01:38:12 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,6 @@ from setuptools import setup, find_packages
 from input_with_timeout import (__title__, __version__, __url__,__license__, __author__,
                                 __classifiers__, __author_email__, __description__)
 
-
-# read the contents of README.md file
-from pathlib import Path
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
-
-
 setup(
     name=__title__                                                              ,
     version=__version__                                                         ,
@@ -18,7 +11,7 @@ setup(
     author=__author__                                                           ,
     author_email=__author_email__                                               ,
     description=__description__                                                 ,
-    long_description=long_description                                           ,
+    long_description=open('README.md', 'rt').read()                             ,
     long_description_content_type='text/markdown'                               ,
     keywords=['python', 'cross-platform', 'stdout', 'stin', 'console']          ,
     classifiers=__classifiers__                                                 ,
